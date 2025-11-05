3a3fc21047b81c2dbb19f9478c777337a03e6124
FranklinMonsalve <computacion5@domosa.com>
2025-11-05 10:55:35 -0400

    sqlpostgres

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,13 +78,11 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+
+#postgresql://postgres:WprgNunViVpWilDRWalAwEaNgevikqSe@shortline.proxy.rlwy.net:20719/railway
 
 
 # Password validation
