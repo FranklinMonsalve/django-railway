e8ea206735350eac5d038d12c04e439de0c75988
FranklinMonsalve <computacion5@domosa.com>
2025-11-17 11:13:25 -0400

    Actual

--- django_project/settings.py
+++ django_project/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-   
+   # 'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+   'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 #postgresql://postgres:WprgNunViVpWilDRWalAwEaNgevikqSe@shortline.proxy.rlwy.net:20719/railway
